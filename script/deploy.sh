b40dcc4cea09710b6d25d1d857ac718b3878f710
cho45 <cho45@lowreal.net>
2016-05-06 15:21:46 +0900

    rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 
 optiwww.rb static/css/style.css
 optiwww.rb static/js/nogag.js
-rsync -av --update --exclude 'session/' --exclude 'db/' --exclude 'service/postprocess-js-daemon/log/main/' /srv/www/lowreal.net/Nogag-beta/  /srv/www/lowreal.net/Nogag
+rsync -av --update --exclude 'session/' --exclude 'db/*.db' --exclude 'service/postprocess-js-daemon/log/main/' /srv/www/lowreal.net/Nogag-beta/  /srv/www/lowreal.net/Nogag
 sudo svc -h /service/backend
 
